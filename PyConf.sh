315f5adcd10c62ea5843dedecbc8ad640b3d6065
BLINDED <mikmikdead@gmail.com>
2023-04-24 22:40:00 +0300

    Update PyConf.sh

--- PyConf.sh
+++ PyConf.sh
@@ -1,55 +1,46 @@
 #!/bin/bash
 
-# Get interface information
-interfaces=$(ip addr show | awk '/inet / {print $2}')
-echo "Available interfaces: "
-echo "$interfaces"
-read -p "Enter the interface name to configure: " interface
+# User inputs
+read -p "Enter the interface name: " interface_name
+read -p "Enter the desired IP address (or type 'dhcp' for DHCP): " ip_address
+read -p "Enter the gateway IP address: " gateway_ip
 
-# Get IP address information
-read -p "Enter the IP address to configure (or 'dhcp' to use DHCP): " ip_address
-if [[ "$ip_address" == "dhcp" ]]; then
-    dhcp="true"
+# Automatic netmask selection based on entered IP address
+IFS=. read -r i1 i2 i3 i4 <<< "$ip_address"
+if [ "$i1" -eq 10 ]; then
+  netmask="255.0.0.0"
+elif [ "$i1" -eq 172 -a "$i2" -ge 16 -a "$i2" -le 31 ]; then
+  netmask="255.255.0.0"
+elif [ "$i1" -eq 192 -a "$i2" -eq 168 ]; then
+  netmask="255.255.255.0"
 else
-    dhcp="false"
-    read -p "Enter the netmask to configure (e.g. 255.255.255.0): " netmask
-    read -p "Enter the default gateway to configure: " gateway
+  echo "Invalid IP address entered"
+  exit 1
 fi
 
-# Configure interface
-if [[ "$dhcp" == "true" ]]; then
-    echo "Configuring $interface with DHCP..."
-    dhclient $interface
-else
-    echo "Configuring $interface with static IP address..."
-    echo "auto $interface" >> /etc/network/interfaces
-    echo "iface $interface inet static" >> /etc/network/interfaces
-    echo "  address $ip_address" >> /etc/network/interfaces
-    echo "  netmask $netmask" >> /etc/network/interfaces
-    echo "  gateway $gateway" >> /etc/network/interfaces
-    ifup $interface
+# Set up the networking using systemd-resolved
+cat <<EOF > /etc/systemd/network/${interface_name}.network
+[Match]
+Name=${interface_name}
+
+[Network]
+Address=${ip_address}/${netmask}
+Gateway=${gateway_ip}
+EOF
+
+systemctl restart systemd-networkd
+
+# Configure nftables
+read -p "Enter the ports to leave open (comma-separated): " open_ports
+
+if [ -n "$open_ports" ]; then
+  IFS=',' read -ra ports <<< "$open_ports"
+
+  # Set up nftables to allow incoming traffic on the specified ports
+  for port in "${ports[@]}"; do
+    nft add rule inet filter input tcp dport "$port" accept
+  done
 fi
 
-# Get port information
-read -p "Enter the ports to open (e.g. 22,80,443): " open_ports
-echo "Configuring firewall..."
-# Flush existing rules
-nft flush ruleset
-# Configure default policies
-nft add table inet filter
-nft add chain inet filter input { type filter hook input priority 0\; }
-nft add chain inet filter forward { type filter hook forward priority 0\; }
-nft add chain inet filter output { type filter hook output priority 0\; }
-nft add rule inet filter input ct state invalid drop
-nft add rule inet filter input ct state established,related accept
-nft add rule inet filter output ct state established,related accept
-nft add rule inet filter forward ct state established,related accept
-nft add rule inet filter forward ct state invalid drop
-nft add rule inet filter forward iifname lo accept
-# Open specified ports
-for port in $(echo $open_ports | sed "s/,/ /g")
-do
-    nft add rule inet filter input tcp dport $port accept
-done
-# Close all other ports
-nft add rule inet filter input tcp dport 0-65535 drop
+# Set the default nftables policy to drop incoming traffic
+nft add chain inet filter input { type filter hook input priority 0 \; policy drop \; }
